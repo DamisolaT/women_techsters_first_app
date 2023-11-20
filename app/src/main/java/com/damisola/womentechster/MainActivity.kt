6d854b119bc75bd7a65aa909498cb7b8f1343cf3
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2023-11-20 13:55:23 +0100

    Implemented features for November 20th.

--- app/src/main/java/com/damisola/womentechster/MainActivity.kt
+++ app/src/main/java/com/damisola/womentechster/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.damisola.womentechster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,11 +8,13 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
 
-        //val jnBtn: Button = findViewById(R.id.joinNowButton)
-       // jnBtn.setOnClickListener{
-        // startActivity()
-       // }
+        var joinButton = this.findViewById<Button>(R.id.joinNowButton)
+
+        joinButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
