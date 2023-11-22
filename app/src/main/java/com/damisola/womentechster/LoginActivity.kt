e09981eeab926fa1a3bac1ad1895981a1ed4a3c9
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2023-11-22 15:30:58 +0100

    November 22nd features implemented by lunch break

--- app/src/main/java/com/damisola/womentechster/LoginActivity.kt
+++ app/src/main/java/com/damisola/womentechster/LoginActivity.kt
@@ -24,8 +24,8 @@ class LoginActivity : AppCompatActivity() {
                  startActivity(intent)
              }
 
-               val loginButton: Button = findViewById(R.id.loginbutton)
-                  loginButton.setOnClickListener{
+                   val loginButton: Button = findViewById(R.id.loginbutton)
+                   loginButton.setOnClickListener{
                       val intent = Intent(this,LoginActivity::class.java)
                       startActivity(intent)
                   }
